7e212e0331a9102b449035ab378716707d8ae9de
Craig Wann and David Ngo <david@>
2018-06-13 14:12:50 -0700

    add rollDice function, player turns, scoring mechanism and winning score

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 // business logic
-function Player(name, score) {
+function Player() {
   this.name = name;
-  this.score = score;
+  this.score = 0;
   this.currentRolls = [];
 }
 
 Player.prototype.rollDice = function(playerTurn) {
   var roll = Math.floor(Math.random() * 6) + 1;
-  if(roll === 1) {
-    this.currentRolls.push(0);
-    return roll;
-  }
   this.currentRolls.push(roll);
   return roll;
 }
@@ -20,9 +16,12 @@ function getSum(total, num) {
 }
 
 Player.prototype.hold = function() {
-  this.currentRolls = this.currentRolls.reduce(getSum);
+  this.currentRolls[0] = this.currentRolls.reduce(getSum);
+  this.score += this.currentRolls[0];
+  this.currentRolls = [];
+  // 20
+  // ['20']
 }
-
 
 // user logic
 // shorthand option for document ready: $(function() {});
@@ -31,28 +30,96 @@ $(document).ready(function() {
   var playerOne = new Player;
   var playerTwo = new Player;
 
+  var diceToString= ['one', 'two', 'three', 'four', 'five', 'six']
+
+  var diceString;
+
+  // var diceString = diceToString[playerOne.currentRolls() -1]
+
   console.log("playerTurn currently: " + playerTurn);
-  console.log(playerOne);
+  $("#hold").attr("disabled", true);
 
   $("#roll").click(function() {
-    playerOne.rollDice();
-    console.log(playerOne.currentRolls);
-    // if(playerOne.currentRolls[playerOne.currentRolls.length - 1] === 0) {
-    //   console.log("rolled a '1'");
-    //   console.log("before wipe: " + playerOne.currentRolls);
-    //   playerOne.currentRolls = [];
-    //   console.log("after wipe: " + playerOne.currentRolls);
-    if(currentPlayer.currentRolls === [])
-      if(playerTurn === 1) {
-        playerTurn = 2;
-      } else if(playerTurn === 2) {
-        playerTurn = 1;
+
+    $("#hold").attr("disabled", false);
+
+    if(playerTurn === 1) {
+      diceString = diceToString[playerOne.rollDice() - 1];
+      console.log(playerOne.currentRolls);
+      console.log(diceString);
+      if(playerOne.currentRolls[playerOne.currentRolls.length - 1] === 1) {
+        console.log("rolled a '1'");
+        console.log("before wipe: " + playerOne.currentRolls);
+        playerOne.currentRolls = [];
+        console.log("after wipe: " + playerOne.currentRolls);
+        console.log("Player one current score: " + playerOne.score);
+        $("#hold").attr("disabled", true);
+
+        if(playerTurn === 1) {
+          playerTurn = 2;
+        } else if(playerTurn === 2) {
+          playerTurn = 1;
+        }
+
+        console.log("playerTurn currently: " + playerTurn);
       }
-      console.log("playerTurn currently: " + playerTurn);
+
+      if (playerOne.score >= 20) {
+        console.log("Player One Wins");
+        $("#hold").attr("disabled", true);
+        $("#roll").attr("disabled", true);
+      }
+
     }
+    else {
+      diceString = diceToString[playerTwo.rollDice() - 1];
+      console.log(playerTwo.currentRolls);
+
+      if(playerTwo.currentRolls[playerTwo.currentRolls.length - 1] === 1) {
+          console.log("rolled a '1'");
+          console.log("before wipe: " + playerTwo.currentRolls);
+          playerTwo.currentRolls = [];
+          console.log("after wipe: " + playerTwo.currentRolls);
+          console.log("Player two current score: " + playerTwo.score);
+          $("#hold").attr("disabled", true);
+
+          if(playerTurn === 1) {
+            playerTurn = 2;
+          } else if(playerTurn === 2) {
+            playerTurn = 1;
+          }
+
+          console.log("playerTurn currently: " + playerTurn);
+      }
+
+      if (playerTwo.score >= 20) {
+        console.log("Player Two Wins");
+        $("#hold").attr("disabled", true);
+        $("#roll").attr("disabled", true);
+      }
+
+    }
+
+    $("#dice").html("<i class='fas fa-dice-" + diceString + " fa-10x'></i>");
+
   });
 
-  $("#dice").append("<i class='fas fa-dice-six fa-10x'></i>");
+  $("#hold").click(function() {
+    $("#hold").attr("disabled", true);
+    if (playerTurn === 1) {
+      playerOne.hold()
+      console.log("Player one current score: " + playerOne.score);
+    } else if(playerTurn === 2) {
+      playerTwo.hold();
+      console.log("Player two current score: " + playerTwo.score);
+    }
+    if(playerTurn === 1) {
+      playerTurn = 2;
+    } else if(playerTurn === 2) {
+      playerTurn = 1;
+    }
+    console.log("playerTurn currently: " + playerTurn);
+  })
 
 
 });
